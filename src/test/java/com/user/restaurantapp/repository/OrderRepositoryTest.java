ae556063655f52aca2478637325e6b9561831bef
Daniel Opara <oparadaniv@gmail.com>
2024-05-12 15:40:46 +0100

    completed repository test for order repository

--- src/test/java/com/user/restaurantapp/repository/OrderRepositoryTest.java
+++ src/test/java/com/user/restaurantapp/repository/OrderRepositoryTest.java
@@ -42,4 +42,39 @@ public class OrderRepositoryTest {
         //assert
         Assertions.assertThat(orderSaved).isNotNull();
     }
+
+    @Test
+    public void OrderRepository_GetAll_Test(){
+        //arrange
+        Order order = new Order();
+        OrderedItems orderedItems = new OrderedItems();
+        orderedItems.setFoodName("Beans");
+        orderedItems.setPortion(1);
+        orderedItems.setOrder(order);
+
+
+        order.setPersonName("John");
+        order.setRefNo("12312");
+        order.setOrderList(Arrays.asList(orderedItems));
+
+        Order order1 = new Order();
+        OrderedItems orderedItems1 = new OrderedItems();
+        orderedItems1.setFoodName("Beans");
+        orderedItems1.setPortion(1);
+        orderedItems1.setOrder(order);
+
+
+        order1.setPersonName("John");
+        order1.setRefNo("12312");
+        order1.setOrderList(Arrays.asList(orderedItems));
+
+        // act
+        orderRepository.save(order);
+        orderRepository.save(order1);
+        List<Order> allOrders = orderRepository.findAll();
+
+        //assert
+        Assertions.assertThat(allOrders).isNotNull();
+        Assertions.assertThat(allOrders.size()).isGreaterThan(1);
+    }
 }
